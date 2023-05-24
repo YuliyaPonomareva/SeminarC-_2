9737392f19d204f2068f661dff06e92aa736af5a
yuliya <liana-yuliana@list.ru>
2023-05-24 21:36:40 +0300

    MiddleDidgit

---
+++ task010/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int EnterDigit()
+{
+    Console.WriteLine("Введите трёхзначное число: ");
+    int n = int.Parse(Console.ReadLine()!);
+    return n;
+}
+int FirstDigit (int n)
+{
+    int num = n / 10;
+    return num;
+}
+
+int MiddleDigit (int num)
+{
+    int res = num % 10;
+    return res;
+}
+
+void FindMiddle (int res)
+{
+    
+    Console.WriteLine("Вторая цифра этого числа: " + res);
+}
+
+int n = EnterDigit();
+int num = FirstDigit(n);
+int res = MiddleDigit(num);
+FindMiddle(res);
